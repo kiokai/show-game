3d2d8d8cd739939a324ca1295d9635953afe7f97
kiokai <110050229+kiokai@users.noreply.github.com>
2023-07-06 20:27:44 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,100 @@
+function createArrow(id) {
+  const i = document.createElement("i")
+  i.className = `fas fa-arrow-${id}`
+
+  return i
+}
+
+function generateArray() {
+  const lists = []
+  const arrow = ["up", "down", "left", "right"]
+
+  for (var i = 0; i < 10; i++) {
+    const rand = Math.floor(Math.random() * 3)
+    lists.push({
+      id: arrow[rand]
+    })
+  }
+
+  return lists
+}
+
+function generateArrow() {
+  document.getElementById("show").innerHTML = ""
+  const lists = generateArray()
+
+  for (var list of lists) {
+    const i = createArrow(list.id)
+    const div = document.createElement("div")
+
+    div.appendChild(i)
+    document.getElementById("show").appendChild(div)
+
+  }
+
+  return lists
+}
+
+function coutdown() {
+
+}
+
+function Game(lists) {
+  var start = 0
+  const control = document.querySelectorAll("#button button")
+  const show = document.querySelectorAll("#show div")
+
+
+  function check(id) {
+    if (lists[start].id == id) {
+      show[start].style.color = "green"
+      start++
+      
+      if(start == 10){
+        text.innerHTML = "You Win"
+        clearInterval(gs)
+        cout = 10
+        buttonStart.style.display = "block"
+      }
+    }
+  }
+
+  for (var i of control) {
+    i.onclick = function() {
+      check(this.id)
+    }
+  }
+}
+
+var cout = 10
+var gs
+const text = document.getElementById("text")
+const buttonStart = document.getElementById("start")
+
+window.onload = function() {
+  
+  buttonStart.onclick = () => {
+    buttonStart.style.display = "none"
+    text.innerHTML = ""
+
+    const lists = generateArrow()
+    const time = document.getElementById("time")
+    
+
+    Game(lists)
+
+    gs = setInterval(() => {
+      time.innerHTML = cout
+
+      cout--
+
+      if (cout < 0) {
+        clearInterval(gs)
+        cout = 10
+        
+        text.innerHTML = "You Lose"
+        buttonStart.style.display = "block"
+      }
+    }, 1000)
+  }
+}
